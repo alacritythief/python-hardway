6f252982b87583fef39e597abdf4053e12e7e01f
Andy Wong <awong@cogolabs.com>
2014-12-01 14:36:36 -0500

    Access page denial if user is not logged in.

--- real-python/flask-blog/blog.py
+++ real-python/flask-blog/blog.py
@@ -3,7 +3,9 @@
 # imports
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
+from functools import wraps
 import sqlite3
+
 
 # configuration
 DATABASE = 'blog.db'
@@ -20,6 +22,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 # routes
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -34,6 +46,7 @@ def login():
     return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
