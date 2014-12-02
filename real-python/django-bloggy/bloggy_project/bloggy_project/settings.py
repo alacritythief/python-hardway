4ddde5fbc5a7e02a62115f386767adab8e323487
Andy Wong <awong@cogolabs.com>
2014-12-02 12:24:58 -0500

    migrations and model

--- real-python/django-bloggy/bloggy_project/bloggy_project/settings.py
+++ real-python/django-bloggy/bloggy_project/bloggy_project/settings.py
@@ -30,12 +30,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,9 +63,10 @@ WSGI_APPLICATION = 'bloggy_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'bloggy.db'),
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
